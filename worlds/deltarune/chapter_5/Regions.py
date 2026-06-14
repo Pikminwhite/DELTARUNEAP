6370283a41ace6f00140d68dbfba0c0a844936ec
Tenebrosful <tenebrosful@proton.me>
2026-06-14 15:05:38 +0200

    Update Regions.py

--- worlds/deltarune/chapter_5/Regions.py
+++ worlds/deltarune/chapter_5/Regions.py
@@ -1,14 +1,5 @@
 from typing import TYPE_CHECKING
 
-from BaseClasses import Region
-from rule_builder.field_resolvers import FromOption
-from rule_builder.options import OptionFilter
-from rule_builder.rules import CanReachLocation, Has
-from worlds.deltarune.Options import (
-    ChosenRoute,
-    MacGuffinChapter4,
-    RandomizeSecretBosses,
-)
 from worlds.deltarune.Regions import Regions, add_location_to_region
 from worlds.deltarune.chapter_5.Locations import chapter5_locations
 from worlds.deltarune.Items import items, ItemIDs, glitched_item_name
